78298e0b656fcd65e2bebab9d603b0163e0d0aeb
Dan Soutar <dan.soutar@littlerocket.io>
2020-12-29 19:29:20 -0500

    setting up product list reducer

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,1 +1,23 @@
-export const productListReducer = (state = { products: [] }, action) => {};
+export const productListReducer = (state = { products: [] }, action) => {
+  switch (action.type) {
+    case 'PRODUCT_LIST_REQUEST':
+      return {
+        loading: true,
+        products: [],
+      };
+    case 'PRODUCT_LIST_SUCCESS':
+      return {
+        loading: false,
+        products: action.payload,
+      };
+    case 'PRODUCT_LIST_FAIL':
+      return {
+        loading: false,
+        error: action.payoadload,
+      };
+    default:
+      return {
+        state,
+      };
+  }
+};
