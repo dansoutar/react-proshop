06c17831d9ac697f1382baefe28f19440139ef21
Dan Soutar <dan.soutar@littlerocket.io>
2021-01-09 11:56:25 -0500

    finished listProducts redux flow, added Loader and Message component to HomeScreen | next step: product details redux flow

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,12 +2,17 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
-import products from '../products';
+import Message from '../components/Message.js';
+import Loader from '../components/Loader.js';
 
+import products from '../products';
 import { listProducts } from '../actions/productActions';
 
 const HomeScreen = () => {
   const dispatch = useDispatch();
+
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
 
   useEffect(() => {
     dispatch(listProducts());
@@ -16,15 +21,21 @@ const HomeScreen = () => {
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => {
-          return (
-            <Col key={product._id} sm={12} md={6} l={4} xl={3}>
-              <Product product={product} />
-            </Col>
-          );
-        })}
-      </Row>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : products ? (
+        <Row>
+          {products.map((product) => {
+            return (
+              <Col key={product._id} sm={12} md={6} l={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            );
+          })}
+        </Row>
+      ) : null}
     </>
   );
 };
