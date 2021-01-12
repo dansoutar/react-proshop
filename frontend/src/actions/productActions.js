9cd7b57ad37265b894baa9f1e751b55d53049f90
Dan Soutar <dan.soutar@littlerocket.io>
2021-01-11 20:00:44 -0500

    finished product page redux flow, moving on to Shopping Cart

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -2,6 +2,9 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
 } from '../constants/productConstants.js';
 
 import axios from 'axios';
@@ -16,6 +19,24 @@ export const listProducts = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: PRODUCT_LIST_FAIL,
+      payload:
+        error.repsonse && error.response.data.message
+          ? error.response.data.message
+          : error.response,
+    });
+  }
+};
+
+export const listProductDetails = (match) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST });
+
+    const { data } = await axios.get(`/api/products/${match.params.id}`);
+
+    dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
       payload:
         error.repsonse && error.response.data.message
           ? error.response.data.message
