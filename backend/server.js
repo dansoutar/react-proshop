6c2094753b9165c9adefb0c2e6a1af9075f36b37
Dan Soutar <dan.soutar@littlerocket.io>
2021-01-14 22:54:53 -0500

    cleaned up product routes and controllers, began adding auth endpoint for user login

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/db.js';
 import colors from 'colors';
 
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -12,7 +13,10 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 app.use('/api/products/', productRoutes);
+app.use('/api/users/', userRoutes);
 
 // create middleware to control status codes
 app.use(notFound);
